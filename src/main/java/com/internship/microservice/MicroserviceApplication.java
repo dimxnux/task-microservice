157df26779c91fbaa1cd2174a329dbec3c7e8c1d
dimxnux <ddarii866@gmail.com>
2022-12-06 15:25:43 +0200

    disable web security on end-points

--- src/main/java/com/internship/microservice/MicroserviceApplication.java
+++ src/main/java/com/internship/microservice/MicroserviceApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableWebSecurity
 @ConfigurationPropertiesScan
 public class MicroserviceApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceApplication.class, args);
+    }
 }
