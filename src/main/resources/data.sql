874e3c24bab20fdce9616edbeca8fc18c8b84706
dimxnux <ddarii866@gmail.com>
2022-12-06 16:23:09 +0200

    update databases sequence table after insertions

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,8 @@
-insert into databases (id, url, username, encrypted_password, driver_class_name)
-values (1, 'jdbc:postgresql://localhost:5432/db1', 'postgres',
+INSERT INTO databases (id, url, username, encrypted_password, driver_class_name)
+VALUES (1, 'jdbc:postgresql://localhost:5432/db1', 'postgres',
         '\xCE628C0ABAFEBAAB9851C2769357CB290D83A82795C56C4007BE89F9BD94F005734318F4', 'org.postgresql.Driver'),
        (2, 'jdbc:postgresql://localhost:5432/db2', 'postgres',
         '\xD02DA48C3ACCA37E639F9532487646A7D8F8513AD567CB1E6897B077D63939CD8782BAAF', 'org.postgresql.Driver');
+
+ALTER TABLE databases
+    ALTER COLUMN id RESTART WITH 3;
