6d7ec73d73963f34f790e646c5c60ccb7716ceaa
dimxnux <ddarii866@gmail.com>
2022-12-09 17:45:46 +0200

    implement user model along with user validation

---
+++ src/main/java/com/internship/microservice/model/User.java
@@ -0,0 +1,104 @@
+package com.internship.microservice.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.time.LocalDate;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class User {
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Pattern(regexp = "[MF]")
+    private String sex;
+
+    private LocalDate dateOfBirth;
+
+    @Pattern(regexp = "[A-Z]{2}")
+    @NotNull
+    private String nationality;
+
+    @NotBlank
+    private String userName;
+
+    @NotBlank
+    private String password;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String sex, LocalDate dateOfBirth, String nationality,
+                String userName, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.sex = sex;
+        this.dateOfBirth = dateOfBirth;
+        this.nationality = nationality;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
