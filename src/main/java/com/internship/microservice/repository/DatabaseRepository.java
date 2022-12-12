48526d7c395dcd152e853f722669258f9ee4d911
dimxnux <ddarii866@gmail.com>
2022-12-12 16:35:48 +0200

    fix DatabaseRepository id type of entity

--- src/main/java/com/internship/microservice/repository/DatabaseRepository.java
+++ src/main/java/com/internship/microservice/repository/DatabaseRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import javax.transaction.Transactional;
 import java.util.Optional;
 
-public interface DatabaseRepository extends JpaRepository<Database, Long> {
+public interface DatabaseRepository extends JpaRepository<Database, String> {
     Optional<Database> findByName(String name);
     @Transactional
     void deleteByName(String name);
