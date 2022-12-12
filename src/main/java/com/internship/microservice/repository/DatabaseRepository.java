ab15979cfd3617dba94e6b26946fa3455fea46ce
dimxnux <ddarii866@gmail.com>
2022-12-12 13:49:30 +0200

    fix database deletion by adding @Transactional

--- src/main/java/com/internship/microservice/repository/DatabaseRepository.java
+++ src/main/java/com/internship/microservice/repository/DatabaseRepository.java
@@ -3,9 +3,11 @@ package com.internship.microservice.repository;
 import com.internship.microservice.model.Database;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 public interface DatabaseRepository extends JpaRepository<Database, Long> {
     Optional<Database> findByName(String name);
+    @Transactional
     void deleteByName(String name);
 }
