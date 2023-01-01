bcb546232804ee56a4e974164a993d407758b946
dimxnux <ddarii866@gmail.com>
2023-01-02 00:01:26 +0200

    add multi-row insert support for users insertion

--- src/main/java/com/internship/microservice/repository/UserRepository.java
+++ src/main/java/com/internship/microservice/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.internship.microservice.repository;
 
 import com.internship.microservice.exception.DuplicateUserNameException;
 import com.internship.microservice.model.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -12,7 +13,8 @@ import java.util.Optional;
 
 @Component
 public class UserRepository {
-    private static final int BATCH_SIZE = 10;
+    @Value("${app.jdbc.users.batch-size}")
+    private int BATCH_SIZE;
     private static final String ALL_USER_COLUMNS =
             "user_name, first_name, last_name, sex, date_of_birth, nationality, password";
     private final JdbcTemplate jdbcTemplate;
@@ -22,6 +24,34 @@ public class UserRepository {
     }
 
     public void addUsers(List<User> users) {
+        final String sqlBaseInsert = "INSERT INTO users (" + ALL_USER_COLUMNS + ") VALUES ";
+        final String parameterPlaceholders = "(?, ?, ?, ?, ?, ?, ?)";
+        final String rowSeparator = ",";
+
+        StringBuilder builder = new StringBuilder(sqlBaseInsert);
+        List<Object> queryArgs = new ArrayList<>();
+        int i = 1;
+        for (User user : users) {
+            checkUserForUsernameDuplicate(user);
+            builder.append(parameterPlaceholders).append(rowSeparator);
+            addUserFieldsToArgsList(user, queryArgs);
+
+            if (i % BATCH_SIZE == 0) {
+                builder.deleteCharAt(builder.length() - 1);
+                jdbcTemplate.update(builder.toString(), queryArgs.toArray());
+                builder = new StringBuilder(sqlBaseInsert);
+                queryArgs.clear();
+            }
+            ++i;
+        }
+
+        if (!queryArgs.isEmpty()) {
+            builder.deleteCharAt(builder.length() - 1);
+            jdbcTemplate.update(builder.toString(), queryArgs.toArray());
+        }
+    }
+
+    public void addUsersBatch(List<User> users) {
         String sqlInsertUser =
                 "INSERT INTO users (" + ALL_USER_COLUMNS + ") VALUES (?, ?, ?, ?, ?, ?, ?)";
 
@@ -38,6 +68,16 @@ public class UserRepository {
             ++i;
         }
         jdbcTemplate.batchUpdate(sqlInsertUser, list);
+    }
+
+    private void addUserFieldsToArgsList(User user, List<Object> destination) {
+        destination.add(user.getUserName());
+        destination.add(user.getFirstName());
+        destination.add(user.getLastName());
+        destination.add(user.getSex());
+        destination.add(user.getDateOfBirth());
+        destination.add(user.getNationality());
+        destination.add(user.getPassword());
     }
 
     private Object[] mapUserToObjectArray(User user) {
